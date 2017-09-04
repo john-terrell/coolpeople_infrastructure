32ea750eb5744133c4f2357ee4ffdcc517cbb191
John W. Terrell <john@coolpeople.us>
2017-09-03 19:55:52 -0700

    Update bootstrap_smartos.sh

--- bootstrap_smartos.sh
+++ bootstrap_smartos.sh
@@ -5,14 +5,14 @@ BOOTSTRAP_TAR="bootstrap-2017Q2-x86_64.tar.gz"
 BOOTSTRAP_SHA="76395983001441108c3ca3ed77d6e071387cc2f5"
 
 # Download the bootstrap kit to the current directory.
-curl -O https://pkgsrc.joyent.com/packages/SmartOS/bootstrap/${BOOTSTRAP_TAR}
+curl -k -O https://pkgsrc.joyent.com/packages/SmartOS/bootstrap/${BOOTSTRAP_TAR}
 
 # Verify the SHA1 checksum.
 [ "${BOOTSTRAP_SHA}" = "$(/bin/digest -a sha1 ${BOOTSTRAP_TAR})" ] || echo "ERROR: checksum failure"
 
 # Verify PGP signature.  This step is optional, and requires gpg.
-curl -O https://pkgsrc.joyent.com/packages/SmartOS/bootstrap/${BOOTSTRAP_TAR}.asc
-curl -sS https://pkgsrc.joyent.com/pgp/DE817B8E.asc | gpg --import
+curl -k -O https://pkgsrc.joyent.com/packages/SmartOS/bootstrap/${BOOTSTRAP_TAR}.asc
+curl -k -sS https://pkgsrc.joyent.com/pgp/DE817B8E.asc | gpg --import
 gpg --verify ${BOOTSTRAP_TAR}{.asc,}
 
 # Install bootstrap kit to /opt/local
